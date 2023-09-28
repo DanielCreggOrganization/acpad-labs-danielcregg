af21e2612e4daf0ca50aa18cbb1fec031a4033ba
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-09-28 10:26:09 +0000

    Lab 1 done

---
+++ lab1-structure/src/app/home/home.page.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(private alertController: AlertController, private navCtrl: NavController) {}
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Hello!',
+      message: 'This is an alert!',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  goToAbout() {
+    this.navCtrl.navigateForward('/about');
+  }
+}
