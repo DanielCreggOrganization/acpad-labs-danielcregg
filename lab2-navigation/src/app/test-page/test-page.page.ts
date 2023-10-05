1cf2f81ca2e35267963aeec654f75bda521eaf02
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-10-05 10:35:29 +0000

    Added router url button to test page

--- lab2-navigation/src/app/test-page/test-page.page.ts
+++ lab2-navigation/src/app/test-page/test-page.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-test-page',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestPagePage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  // Pop the 'test-page' off the navigation stack
+  popTestPageoffStack() {
+    this.router.navigateByUrl('/home');
   }
 
 }
