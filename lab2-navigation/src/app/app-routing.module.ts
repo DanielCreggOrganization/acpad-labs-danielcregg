efac283cc6b49e9c40ba4535b5e0b823be93abfb
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-10-06 10:25:14 +0000

    Added more routing

--- lab2-navigation/src/app/app-routing.module.ts
+++ lab2-navigation/src/app/app-routing.module.ts
@@ -1,21 +1,35 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { TabsComponent } from './tabs/tabs.component';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
+    // The first route in the configuration is the default route. 
+    // This is the route that your app will navigate to when the user first visits your app.
+    // The default route in this case is the TabsComponent.
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    component: TabsComponent,
+    // The children property of the TabsComponent route defines the pages that the tabs correspond to. 
+    // In this case, the tabs correspond to the HomePageModule and the TestPagePageModule.
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home',
+      },
+      {
+        path: 'home',
+        loadChildren: () => import('./home/home.module').then((m) => m.HomePageModule),
+      },
+      {
+        path: 'products',
+        loadChildren: () => import('./products/products.module').then( m => m.ProductsPageModule)
+      },
+    ],
   },
-  {
-    path: 'products',
-    loadChildren: () => import('./products/products.module').then( m => m.ProductsPageModule)
-  },
+
 ];
+
 
 @NgModule({
   imports: [
