f92f09267a5a895a1b2feb0fa74ccc26e339f723
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-10-12 10:38:16 +0000

    added CRUD

---
+++ lab3-data/src/app/home/home.page.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  // Define items property
+  items: { key: string, value: string }[] = [];
+  key = '';
+  value = '';
+  removeItemKey = '';
+  getItemKey = '';
+  itemValue = '';
+
+  constructor(private storage: Storage) {
+    this.initStorage(); // Call initStorage method on construction
+  }
+
+  async initStorage() {
+    await this.storage.create(); // Create storage instance
+  }
+
+  async setItem() {
+    await this.storage.set(this.key, this.value); // Set item in storage
+    this.key = ''; // Clear key property
+    this.value = ''; // Clear value property
+    await this.loadItems(); // Load items from storage
+  }
+
+  async removeItem() {
+    await this.storage.remove(this.removeItemKey); // Remove item from storage
+    this.removeItemKey = ''; // Clear removeItemKey property
+    await this.loadItems(); // Load items from storage
+  }
+
+  async removeListItem(item: { key: string, value: string }) {
+    await this.storage.remove(item.key); // Remove item from storage
+    await this.loadItems(); // Load items from storage
+  }
+
+  async getItem() {
+    const value = await this.storage.get(this.getItemKey); // Get item from storage
+    this.itemValue = value; // Set item value to itemValue property
+  }
+
+  async loadItems() {
+    // Create a temporary array to store items
+    const itemsTempStorage: { key: string, value: string }[] = [];
+    // Add item to items array
+    await this.storage.forEach((value, key) => {
+      // Push key and value to temporary storage
+      itemsTempStorage.push({ key: key, value: value });
+    });
+    // Save items property to items array so that it can be displayed in the view
+    this.items = itemsTempStorage;
+  }
+
+}
