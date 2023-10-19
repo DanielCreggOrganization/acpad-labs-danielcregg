b0ea746d22d3c608d43e4dc7255cebdf190d9389
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-10-19 10:25:20 +0000

    most bacic standalone

---
+++ lab4-ngmodules/src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+     // Add path to standalone component. Any route can lazily load its routed, standalone component by using loadComponent:
+     path: 'standalone',
+     loadComponent: () => import('./standalone/standalone.component').then(mod => mod.StandaloneComponent)
+  },
+  {
+    path: 'ngcomponent',
+    loadChildren: () => import('./ngmodule/ngmodule.module').then(m => m.NgmoduleModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
